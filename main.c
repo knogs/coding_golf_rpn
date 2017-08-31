40d9044d14a004022939628d0ded28a861de8476
Roger Knecht <rkne@zuehlke.com>
2017-08-31 18:42:59 +0200

    152

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 i = 1;
 s;
 #define f atoi(a[i++]);
-#define b(c,o) case 40+c: s o f break;
+#define b(c,o) case 40+c: s = s o f break;
 main(c, a)char**a; {
     for(s = f i<c;i++) {
         switch (*a[i+1]) {
-            b(3, +=)
-            b(5, -=)
-            b(2, *=)
-            b(7, /=)
+            b(3, +)
+            b(5, -)
+            b(2, *)
+            b(7, /)
         }
     }
 
