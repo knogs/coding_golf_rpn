6259563ad528e759c805ad9be3ddff6f47142179
Roger Knecht <rkne@zuehlke.com>
2017-08-31 17:55:05 +0200

    180

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 i = 1;
 s;
+x;
 main(int c, char**a) {
     s = atoi(a[i++]);
 
     while(i<c) {
-        int x = atoi(a[i++]);
+        x = atoi(a[i++]);
 
         switch (*a[i++]) {
             case '+': s += x; break;
