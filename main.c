7536b6eaa67b94008ccc3807ba06dabe036b8b6c
Roger Knecht <rkne@zuehlke.com>
2017-09-04 11:48:52 +0200

    151

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
-s, i = 1;
-#define f atoi(a[i++]);
+s;
+#define f atoi(*(++a));
 #define b(c,o) case 40+c: s = s o f break;
 main(c, a)char**a; {
-    for(s = f i<c;i++) {
-        switch (*a[i+1]) {
+    for(s = f c>2; c-=2) {
+        switch (*a[2]) {
             b(3, +)
             b(5, -)
             b(2, *)
             b(7, /)
         }
+        a++;
     }
 
     printf("%d", s);
