49d58e62252c71e7680174838a018e63b0d7b5b0
Roger Knecht <rkne@zuehlke.com>
2017-09-06 09:17:20 +0200

    121

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 s;
-#define f atoi(*(++a));
-#define b(o) if (*a[2] == *#o) s = s o f else
+#define f atoi(*(++a))
+#define b(o) (*a[2] == *#o) ? s o f :
 main(c, a)char**a; {
-    for(s = f c-=2; a++)
-        b(+)
+    for(s = f; c-=2; a++)
+        s = b(+)
         b(-)
         b(*)
         b(/)
-        ;
+        0;
 
     printf("%d", s);
 }
