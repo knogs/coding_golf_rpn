51a3b5f97fbdcf75f0d7f24927b827558ffac026
Roger Knecht <rkne@zuehlke.com>
2017-09-06 10:46:55 +0200

    cleanup

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #define F(o) *v[1] == *#o ? c o atoi(*v) :
-main(c, v)char**v; {
+main(c, v) char**v; {
     for(; *v; v+=2)
         c = F(+)
-        F(-)
-        F(*)
-        F(/)
-        atoi(v[1]);
+            F(-)
+            F(*)
+            F(/)
+            atoi(v[1]);
 
     printf("%d", c);
 }
