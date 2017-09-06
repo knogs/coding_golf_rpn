71f4f4d36aa2245a2d78d7d59b25a7e8d37dba82
Roger Knecht <rkne@zuehlke.com>
2017-09-06 11:06:37 +0200

    107

--- main.c
+++ main.c
@@ -1,11 +1,5 @@
 #define F(o) *v[1] == *#o ? c o atoi(*v) :
 main(c, v) char**v; {
-    for(; *v; v+=2)
-        c = F(+)
-            F(-)
-            F(*)
-            F(/)
-            atoi(v[1]);
-
-    printf("%d", c);
+    *v ? main(F(+) F(-) F(*) F(/) atoi(v[1]), v+2) 
+       : printf("%d", c);
 }
